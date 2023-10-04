bdd7544a52d5f721269c336def84e7b85e644381
Charles Rai <charles.raitsev@gmail.com>
2023-10-04 08:22:59 -0600

    display

---
+++ display/display.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(int argc, char *argv[]){
+    if(argc != 5){
+        pritnf("Arg")
+        return 1;
+    }
+
+    int coord[8];
+
+    for(int i = 0; argv[i] != '\0'; i++{
+        coord[i] = argv[i] - 
+    })
+
+
+
+}

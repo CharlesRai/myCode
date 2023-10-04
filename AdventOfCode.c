995268be08cb35d331e929c8c7c4522bb6c98e3f
CharlesRai <142817709+CharlesRai@users.noreply.github.com>
2023-10-03 20:08:14 -0600

    Update AdventOfCode.c

--- AdventOfCode.c
+++ AdventOfCode.c
@@ -1,1 +1,33 @@
+#include <stdio.h>
+#include <cs50.h>
+#include <string.h>
 
+#define FILESIZE 100
+
+int main(int argc, string argv[]){
+
+    if(argc != 2){
+        printf("Too few arguments");
+        return 1;
+    } else if (argc > 2){
+        printf("Too many arguments");
+        return 1;
+    }
+
+    string filename = argv[1];
+
+    char aov_filename[10];
+
+    sprintf(aov_filename, "%s.txt", filename);
+
+    FILE *file = fopen(filename, "r");
+    if(file == NULL){
+        printf("File is NULL");
+        return 1;
+    }
+
+    char options[FILESIZE][100];
+    for(int i = 0; i < FILESIZE; i++){
+        fscanf(file, "%s", options[i]);
+    }
+}
